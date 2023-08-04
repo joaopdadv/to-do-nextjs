38a36951ee51e387c8ffb556739fa31c1cf3e3c3
joaopdadv <joaopdadv@gmail.com>
2023-08-04 15:17:44 -0300

    primeiro commit

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+type TodoItemProps = {
+    id: string
+    title: string
+    complete: boolean
+    toggleTodo: (id: string, complete:boolean) => void
+}
+
+export default function TodoItem({id, title, complete, toggleTodo}: TodoItemProps){
+    return <li className="flex ap-1 items-center">
+        <input id={id} type="checkbox" defaultChecked={complete} onChange={e => toggleTodo(id, e.target.checked)} className="cursor-pointer peer"/>
+        <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
+    </li>
+}
